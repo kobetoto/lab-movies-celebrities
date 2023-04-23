98a46032984231ade9da72ed545ddd88782106ae
rachelpytse <rachelpytse@gmail.com>
2023-04-23 15:15:38 +0200

    probleme sur iteration 6. Fini iteration 7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -8,31 +8,29 @@ router.get("/movies/create", function (req, res, next) {
   Celebrity.find()
     .then(function (allCelebritiesFromDB) {
       console.log("All celebritiesssss", allCelebritiesFromDB);
-      res.render("movies/new-movie", {allCelebrities: allCelebritiesFromDB});
+      res.render("movies/new-movie", { allCelebrities: allCelebritiesFromDB });
     })
     .catch((err) => {
       console.log("oops", err);
       next(err);
     });
-  
 });
 
 router.post("/movies/create", function (req, res, next) {
-  
-
   new Movie({
     title: req.body.title,
     genre: req.body.genre,
     plot: req.body.plot,
-    cast: req.body.cast
+    cast: [req.body.cast],
   })
     .save()
     .then(function (newMovieFromDB) {
+      console.log("movies created");
       res.redirect("/movies");
     })
     .catch((err) => {
-      console.log("erreur lorsque de la creation d un movie");
-      res.render("movies/new-movie");
+      console.log("erreur lorsque de la creation d un movie", err);
+      res.render("movies/new-movie", {});
     });
 });
 
@@ -42,7 +40,7 @@ router.get("/movies", function (req, res, next) {
       console.log("All moviesssss", allMoviesFromDB);
       res.render("movies/movies", {
         allMovies: allMoviesFromDB,
-      })
+      });
     })
     .catch((err) => {
       console.log("oops", err);
